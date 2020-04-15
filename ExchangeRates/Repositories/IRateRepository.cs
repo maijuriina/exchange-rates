c8ec4b703a8ffaecc5bfdc0387892a17b2dd5aba
Maijuriina Mustonen <maiju.mustonen@gmail.com>
2020-04-15 18:34:41 +0300

    RateRepository and IRateRepository created with Read()

---
+++ ExchangeRates/Repositories/IRateRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.Repositories
+{
+    public interface IRateRepository
+    {
+        List<Rate> Read();
+    }
+}
---
+++ ExchangeRates/Repositories/RateRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExchangeRates.Repositories
+{
+    public class RateRepository : IRateRepository
+    {
+        // inject database for repository's use
+        private readonly ExchangeRatesDbContext _exchangeRatesDbContext;
+
+        public RateRepository(ExchangeRatesDbContext exchangeRatesDbContext)
+        {
+            _exchangeRatesDbContext = exchangeRatesDbContext ?? throw new ArgumentNullException(nameof(exchangeRatesDbContext));
+        }
+
+        public List<Rate> Read()
+        {
+            return _exchangeRatesDbContext.Rate.ToList();
+        }
+    }
+}
